0395d9593aac639036e33e3d148051685b7f149a
Siyun Feng <siyunf21@gmail.com>
2022-11-03 13:03:40 -0400

    first commit

---
+++ components/PostCard.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function PostCard( {post}) {
+  return (
+    <div>
+    {post.title}
+    {post.excerpt}</div>
+  )
+}
+
+export default PostCard
---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: [],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
+
+module.exports = {
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      spacing: {
+        "2/3": "66.666667%",
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};

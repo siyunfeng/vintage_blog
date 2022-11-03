1f0e23713166e6dad3a06995ece6606f48390e4c
Siyun Feng <siyunf21@gmail.com>
2022-11-03 17:51:24 -0400

    Completed the main section for all the posts

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,13 @@
 import Head from "next/head";
-// import Image from "next/image";
-// import styles from "../styles/Home.module.css";
 import { PostCard, Categories, PostWidget } from "../components";
+import { getPosts } from "../services";
 
-const posts = [
-  { title: "React Testing", excerpt: "Learn React Testing" },
-  { title: "React with Tailwind", excerpt: "Learn React with Tailwind" },
-];
+// const posts = [
+//   { title: "React Testing", excerpt: "Learn React Testing" },
+//   { title: "React with Tailwind", excerpt: "Learn React with Tailwind" },
+// ];
 
-export default function Home() {
+export default function Home({ posts }) {
   return (
     // <div className={styles.container}>
     // mx: margin horizontal axis; px: padding horizontal axis; mb: margin bottom
@@ -24,7 +23,7 @@ export default function Home() {
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
         <div className="lg:col-span-8 col-span-1">
           {posts.map((post) => (
-            <PostCard post={post} key={post.title} />
+            <PostCard post={post.node} key={post.title} />
           ))}
         </div>
         <div className="lg:col-span-4 col-span-1">
@@ -36,4 +35,12 @@ export default function Home() {
       </div>
     </div>
   );
+}
+
+// fetch data using getStaticProps in next.js:
+export async function getStaticProps() {
+  const posts = (await getPosts()) || [];
+  return {
+    props: { posts },
+  };
 }
